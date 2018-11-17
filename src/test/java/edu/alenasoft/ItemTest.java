6b563eb993fb84eeef3f29e22f12730ceb2cb217
Luis Roberto Perez Rios <luis.robertop87@gmail.com>
2018-11-17 09:44:05 -0400

    Preparacion base, incluido JaCoCo

---
+++ src/test/java/edu/alenasoft/ItemTest.java
@@ -0,0 +1,14 @@
+package edu.alenasoft;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ItemTest {
+
+  @Test
+  public void simpleTest() {
+    assertTrue(Boolean.TRUE);
+  }
+
+}
